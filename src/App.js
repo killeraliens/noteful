c009818900ab4b392ae9db413265810f811ebf8d
killeraliens <killeraliens@outlook.com>
2019-09-24 19:10:36 -0700

    Not Found Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SideNav from './SideNav/SideNav';
 import NoteList from './NoteList/NoteList';
 import NoteShow from './NoteShow/NoteShow';
 import Header from './Header/Header';
+import NotFoundPage from './NotFoundPage/NotFoundPage';
 import './App.css';
 import Folders from './dummyStore.js'
 
@@ -31,35 +32,39 @@ class App extends Component {
         <Header/>
         <SideNav folders={folders}/>
         <main>
-          <Route exact path='/' render={(routeProps) => {
-            return(
-              <NoteList
-                notes={notes}
-                { ...routeProps }
-              />
-            )
-          }}/>
-          <Route path='/folder/:folderId' render={(routeProps) => {
-            //console.log('rout props for folder path', routeProps.match)
-            const folder = folders.find(folder => folder.id === routeProps.match.params.folderId)
-            // console.log('matching folder', folder )
-            const folderNotes = notes.filter(note => note.folderId === folder.id);
-            //console.log('folders notes', folderNotes )
-            return(
-              <NoteList
-                notes={folderNotes}
-                { ...routeProps }
-              />
-            )
-          }}/>
-          <Route path='/note/:noteId' render={(routeProps) => {
-            const note = notes.find(note => note.id === routeProps.match.params.noteId)
-            return(
-              <NoteShow
-                note={note}
-              />
-            )
-          }}/>
+          <Switch>
+
+            <Route exact path='/' render={(routeProps) => {
+              return(
+                <NoteList
+                  notes={notes}
+                  { ...routeProps }
+                />
+              )
+            }}/>
+            <Route path='/folder/:folderId' render={(routeProps) => {
+              //console.log('rout props for folder path', routeProps.match)
+              const folder = folders.find(folder => folder.id === routeProps.match.params.folderId)
+              // console.log('matching folder', folder )
+              const folderNotes = notes.filter(note => note.folderId === folder.id);
+              //console.log('folders notes', folderNotes )
+              return(
+                <NoteList
+                  notes={folderNotes}
+                  { ...routeProps }
+                />
+              )
+            }}/>
+            <Route path='/note/:noteId' render={(routeProps) => {
+              const note = notes.find(note => note.id === routeProps.match.params.noteId)
+              return(
+                <NoteShow
+                  note={note}
+                />
+              )
+            }}/>
+            <Route component={NotFoundPage}/>
+          </Switch>
         </main>
       </div>
     );
---
+++ src/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function NotFoundPage(props) {
+  return(
+    <div className="NotFoundPage">
+      {alert("404 Not Found!!")}
+      Not Found
+    </div>
+  )
+}
