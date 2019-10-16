58ea3a602469d55fee5c96ee612b3682be47e092
killeraliens <killeraliens@outlook.com>
2019-10-16 11:27:02 -0700

    ErrorBoundary w/routes working (listen in construct))

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,33 +1,47 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
 
 class ErrorBoundary extends Component {
 
   constructor(props) {
     super(props);
+
+   const { history } = props;
+
+    history.listen((location, action) => {
+      if (this.state.hasError) {
+        this.setState({
+          hasError: false,
+        });
+      }
+    });
+
     this.state = {
       hasError: false
     }
   }
 
   static getDerivedStateFromError(error) {
-    console.log('error!!!', error)
-    return { hasError: true}
+    //console.log('error!!!', error)
+    return { hasError: true, error: error}
   }
 
   render() {
+    const details = this.state.error && this.state.error.message
+      ? <details>{this.state.error.message}</details>
+      : null
 
     if(this.state.hasError) {
       return(
         <div className='ErrorBoundary'>
           <summary>Something went wrong</summary>
-
+          {details}
         </div>
       )
     }
 
     return this.props.children
-    //const errorDetails = this.state.hasError
   }
 }
 
-export default ErrorBoundary;
+export default withRouter(ErrorBoundary);
