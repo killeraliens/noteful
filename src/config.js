c3346ea985838b30ab6147eac3b2cf052a9f2389
killeraliens <killeraliens@outlook.com>
2020-01-01 16:49:49 -0700

    fixed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://desolate-meadow-95722.herokuapp.com`, //|| `http://localhost:8000/api`,
+  API_ENDPOINT: `https://whispering-tundra-52779.herokuapp.com`, //|| `http://localhost:8000/api`,
   API_KEY: process.env.REACT_APP_KEY
 }
